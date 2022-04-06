106f8de73b5abae80cdfb550537f886b2a93bf13
Chris <sealscm@etsu.edu>
2022-04-06 14:30:56 -0400

    Implemented serialization and deserialization with ORM

--- Lab 7/Program.cs
+++ Lab 7/Program.cs
@@ -24,81 +24,27 @@ namespace Lab_7
     {
         static void Main(string[] args)
         {
-            /*
-             //Get path of DB
-             string path = FileRoot.GetDefaultDirectory();
-             string fullPath = path + $"{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}Library.db";
-             QueryBuilder qb = new QueryBuilder(fullPath);
-             using (qb)
-             {
-                 //Single read command example
-                 Author author = qb.Read<Author>(1);
-                 Console.WriteLine("Single Read command\n" + author + "\n\n");
-                 //Read all command example
-                 List<Author> authors = qb.ReadAll<Author>();
-                 Console.WriteLine("Read All command");
-                 foreach (var item in authors)
-                 {
-                     Console.WriteLine(item);
-                 }
-                 Console.WriteLine("\n");
-                 //Create command example
-                 Author newAuthor = new Author();
-                 newAuthor.Id = 4;
-                 newAuthor.FirstName = "John";
-                 newAuthor.Surname = "Belemy";
-                 qb.Create<Author>(newAuthor);
-                 authors = qb.ReadAll<Author>();
-                 Console.WriteLine("Create command:");
-                 foreach (var item in authors)
-                 {
-                     Console.WriteLine(item);
-                 }
-                 Console.WriteLine("\n");
-                 //Update command example
-                 newAuthor.Surname = "Larry";
-                 qb.Update<Author>(newAuthor);
-                 authors = qb.ReadAll<Author>();
-                 Console.WriteLine("Update command:");
-                 foreach (var item in authors)
-                 {
-                     Console.WriteLine(item);
-                 }
-                 Console.WriteLine("\n");
-                 //Delete command example
-                 qb.Delete<Author>(newAuthor);
-                 authors = qb.ReadAll<Author>();
-                 Console.WriteLine("Delete command:");
-                 foreach (var item in authors)
-                 {
-                     Console.WriteLine(item);
-                 }
-
-             }
-             */
-
-            //Readall for Users
             string path = FileRoot.GetDefaultDirectory();
             string fullPath = path + $"{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}Library.db";
             QueryBuilder qb = new QueryBuilder(fullPath);
             using (qb)
             {
+                //Readall command then output into JSON
                 List<Users> users = qb.ReadAll<Users>();
 
                 string output = JsonConvert.SerializeObject(users, Formatting.Indented);
 
                 File.WriteAllText(path + $"{Path.DirectorySeparatorChar}Json{Path.DirectorySeparatorChar}Output.json", output);
-            }
-            
-            using (qb)
-            {
+
+
+
+                //Create command using JSON input
                 string input = File.ReadAllText(path + $"{Path.DirectorySeparatorChar}Json{Path.DirectorySeparatorChar}Input.json");
 
                 Books book = JsonConvert.DeserializeObject<Books>(input);
 
-                Console.WriteLine(book);
+                qb.Create<Books>(book);
             }
-
         }
     }
 }
